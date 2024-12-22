38f838e056f5afca96cc358bfbbb61b0fb1491f2
rajpatidar12 <patidarraj465@gmail.com>
2024-12-22 23:37:48 +0530

    working on directory-tree and path

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,24 +1,19 @@
-import util from "util";
-import child_process from "child_process";
-import fs from "fs/promises";
-import uuid4 from "uuid4";
-import { REACT_PROJECT_COMMAND } from "../Config/serverConfig.js";
-
-const execPromisified = util.promisify(child_process.exec);
+import {
+  createProjectService,
+  getProjectTreeService,
+} from "../service/projectService.js";
 
 export const createProjectController = async (req, res) => {
-  //Create a unique id and then inside the projects folder create a folder with that id
-
-  const projectId = uuid4();
-  console.log("New project id: ", projectId);
-
-  await fs.mkdir(`./projects/${projectId}`);
-
-  //After this call the npm create vite command in the newly created folder
-
-  const response = await execPromisified(REACT_PROJECT_COMMAND, {
-    cwd: `./projects/${projectId}`,
-  });
+  const projectId = await createProjectService();
 
   return res.json({ message: "Project created", data: projectId });
+};
+
+export const getProjectTree = async (req, res) => {
+  const tree = await getProjectTreeService(req.params.projectId);
+  return res.status(200).json({
+    data: tree,
+    success: true,
+    message: "Successfully fetched the tree",
+  });
 };
---
+++ frontend/src/store/activeFileTabStore.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+export const useActiveFileTabStore = create((set) => {
+  return {
+    activeFileTab: null,
+    setActiveFileTab: () => {
+      set({
+        activeFileTab: {
+          path: path,
+          value: value,
+          extension: extensio,
+        },
+      });
+    },
+  };
+});
